4449ddc26450fa72b3cbdfcc930230ef8b0c0e71
aishwaryap14 <aishwarya.potdar123@gamil.com>
2021-04-04 13:42:10 +0530

    Added: user validation test for email

--- src/main/java/com/project/UserValidation.java
+++ src/main/java/com/project/UserValidation.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
     public static final String Name_Pattern="^[A-Z]{1}[a-z]{2,}$";
+    public static final String Email_Pattern="^[A-Za-z]{1,}([.]{1}[0-9a-zA-Z+-]{1,}){0,}[@]{1}" +
+                                        "[0-9a-zA-Z]{1,}[.]{1}[a-zA-z]{2,4}([.]{1}[a-zA-Z]{2,4}?)$";
     public boolean validateFirstname(String fname) {
         Pattern pattern=Pattern.compile(Name_Pattern);
         return pattern.matcher(fname).matches();
@@ -12,5 +14,9 @@ public class UserValidation {
     public boolean validateLastname(String lname) {
         Pattern pattern=Pattern.compile(Name_Pattern);
         return pattern.matcher(lname).matches();
+    }
+    public boolean validateEmail(String email) {
+        Pattern pattern=Pattern.compile(Email_Pattern);
+        return pattern.matcher(email).matches();
     }
 }
--- src/test/java/com/project/userValidationTest.java
+++ src/test/java/com/project/userValidationTest.java
@@ -8,41 +8,41 @@ public class userValidationTest {
     @Test
     public void enterFirstname_whenProper_returnTrue() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("Aishwarya");
+        boolean result= userValidation.validateFirstname("Firstname");
         Assert.assertEquals(true,result);
     }
     @Test
     public void enterFirstname_whenNotProper_returnTrue() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("aishwarya");
+        boolean result= userValidation.validateFirstname("firstname");
         Assert.assertEquals(false,result);
     }
 
     @Test
     public void enterFirstname_whenShort_returnFalse(){
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("Ai");
+        boolean result= userValidation.validateFirstname("Fi");
         Assert.assertEquals(false,result);
     }
 
     @Test
     public void enterFirstname_whenWithSpecialChar_returnFalse() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("Aishw@rya");
+        boolean result= userValidation.validateFirstname("First@name");
         Assert.assertEquals(false,result);
     }
 
     @Test
     public void enterFirstname_whenWithNumericValues_returnFalse() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("Aishwa144");
+        boolean result= userValidation.validateFirstname("First114");
         Assert.assertEquals(false,result);
     }
 
     @Test
     public void enterFirstname_whenWithSpace_returnFalse() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateFirstname("Aishwar ya");
+        boolean result= userValidation.validateFirstname("First name");
         Assert.assertEquals(false,result);
     }
 
@@ -50,13 +50,13 @@ public class userValidationTest {
     @Test
     public void enterLastname_whenProper_returnTrue() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateLastname("Potdar");
+        boolean result= userValidation.validateLastname("Lastname");
         Assert.assertEquals(true,result);
     }
     @Test
     public void enterLastname_whenNotProper_returnTrue() {
         UserValidation userValidation =new UserValidation();
-        boolean result= userValidation.validateLastname("potdar");
+        boolean result= userValidation.validateLastname("lastname");
         Assert.assertEquals(false,result);
     }
     @Test
@@ -86,4 +86,43 @@ public class userValidationTest {
         boolean result= userValidation.validateLastname("Potd ar");
         Assert.assertEquals(false,result);
     }
+
+    //UC3
+    @Test
+    public void enterEmail_whenProper_returnTrue() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname.lastname@yaho.com.in");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void enterEmail_whenNotProper_returnFalse() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname.lastname@yaho.in");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void enterEmail_whenNotProper1_returnFalse() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname..lastname@yaho.com.in");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void enterEmail_whenNotProper2_returnFalse() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname.lastname@@yaho.com.in");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void enterEmail_whenNotProper3_returnFalse() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname.lastname@yaho.com.co.in");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void enterEmail_whenwithSpecialChar_returnFalse() {
+        UserValidation userValidation =new UserValidation();
+        boolean result= userValidation.validateEmail("firstname#.lastname^@yaho.in");
+        Assert.assertEquals(false,result);
+    }
+
 }
